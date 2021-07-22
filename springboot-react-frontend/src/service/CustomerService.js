47aa48c5e468a9ee4c39ce0ff94529259e586d35
Abdelrhman Ahmed Sror <48390586+AbdelrhmanSror@users.noreply.github.com>
2021-07-22 05:34:32 +0200

    Update CustomerService.js

--- springboot-react-frontend/src/service/CustomerService.js
+++ springboot-react-frontend/src/service/CustomerService.js
@@ -2,16 +2,18 @@ import axios from 'axios'
 
 
 
-const CUSTOMER_API_URL="http://localhost:8090/customer/";
+const CUSTOMER_API_URL="http://localhost:8090/customer";
 //suspend fun getFilteredCustomers(@Query("countryCode") countryCode: Int, @Query("state") state: Int): List<Customer>
 //http://192.168.1.11:8090/customer/FilteredCustomers/?countryCode=237&state=0
 class CustomerService{
     getAllCustomers(){
-       return axios.get(CUSTOMER_API_URL+'Customers');
-
+      return axios.get(CUSTOMER_API_URL+'/Customers', 
+        { crossdomain: true }
+         );
+       
     }
     getFilteredCustomers(countryCode,state){
-        return axios.get(CUSTOMER_API_URL+'FilteredCustomers/?countryCode='+countryCode+'&state='+state);
+        return axios.get(CUSTOMER_API_URL+'/FilteredCustomers/?countryCode='+countryCode+'&state='+state,{ crossdomain: true });
  
      }
 }
